a247c4eab17fc910071300ea3b27dfe4dc3e95b3
Glongo <glongooo4@gmail.com>
2022-01-30 20:39:39 +0300

    + folder refactor + ShopTools + Analytics tools

---
+++ Assets/Scripts/Model/Shop/IShop.cs
@@ -0,0 +1,12 @@
+﻿using Tools;
+namespace Model.Shop
+{
+    internal interface IShop
+    {
+        void Buy(string id);
+        string GetCost(string productID);
+        void RestorePurchase();
+        IReadOnlySubscriptionAction OnSuccessPurchase { get; }
+        IReadOnlySubscriptionAction OnFailedPurchase { get; }
+    }
+}
