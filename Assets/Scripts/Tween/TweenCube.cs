63e19a967ff99621716d172e8d7ee8f41f355225
Maksim.S <maxsic85@mail.ru>
2022-05-01 21:12:04 +0500

    Add Tween Sceene

--- Assets/Scripts/Tween/TweenCube.cs
+++ Assets/Scripts/Tween/TweenCube.cs
@@ -1,14 +1,32 @@
 using UnityEngine;
 using DG.Tweening;
+using System.Collections.Generic;
+
+[RequireComponent(typeof(Renderer))]
 public class TweenCube : MonoBehaviour
 {
     [SerializeField]
     private float _duration;
     [SerializeField]
-    private Vector3 _endValue;
-    private void Start()
+    private int _countLoops;
+    [SerializeField]
+    private float _positionX;
+    [SerializeField]
+    private float _endScale;
+    private void ComplexTween()
     {
-        transform.DOMove(_endValue, _duration).From();
-
+        Sequence sequence = DOTween.Sequence();
+        sequence.Append(transform.DOMoveY(_positionX,
+        _duration).SetLoops(_countLoops).SetEase(Ease.InExpo));
+        sequence.Insert(0, transform.DOScale(_endScale, _duration));
+        sequence.Insert(1, transform.DOJump(Vector3.forward, 5, 5, _duration));
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.K))
+            ComplexTween();
     }
 }
+
+
+
